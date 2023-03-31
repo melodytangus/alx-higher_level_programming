936865b89bbb6ea501448f04988671fad063998a
melodytangus <melodytangus@gmail.com>
2023-03-31 18:17:00 +0300

    classes and objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -21,6 +21,23 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+    @property
+    def position(self):
+        return self.__position
+    @position.setter
+    def position(self, value):
+        if type(position) != tuple and len(position) != 2 and position[0] < 0 and position [1] <0 :
+            raise TypeError("position must be a tuple of 2 positive integers") 
+        self.__position = value
 
     def area(self):
         return (self.size * self.size)
+    def my_print(self):
+         if self.__size == 0:
+             print("") 
+             return
+         [print("") for i in range(0, self.__position[1])]
+          for i in range(0, self.__size):
+              [print(" ", end="") for j in range(0, self.__position[0])]
+              [print("#", end="") for k in range(0, self.__size)]
+              print ("") 
